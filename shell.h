e8b3ecb44efe9f4a0fb3e28c34a38f040de2c217
ralexrivero <ralexrivero@gmail.com>
2021-08-14 12:59:13 -0400

    prototypes to header

--- shell.h
+++ shell.h
@@ -1,8 +1,10 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+/* start buffer to use or realloc */
 #define BUFFER_SIZE 1024
 
+/* libraries */
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -10,5 +12,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <signal.h>
+
+/* the basic shell lifetime */
+int shell_initialize(void);
+char *shell_interprete(void);
+int shell_terminate(void);
+
+/* the shell commands */
+
+/* built-in commands */
+
 
 #endif
