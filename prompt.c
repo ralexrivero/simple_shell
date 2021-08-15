0102f761db7b03f370344e6c5f1799700160f270
ralexrivero <ralexrivero@gmail.com>
2021-08-14 22:21:14 -0400

    simple

--- prompt.c
+++ prompt.c
@@ -12,14 +12,16 @@
 int main(int argc, char **argv, char **env)
 {
         /* initialize the shell */
-        shell_initialize();
+        // shell_initialize();
         /* loop waiting for commands */
         shell_interprete();
         /* return 0 to terminate the shell and free */
-        shell_terminate();
+        // shell_terminate();
         return (EXIT_SUCCESS);
 }
-
+void shell_initialize(void)
+{
+}
 /**
  * @brief shell interprete
  * 
@@ -27,60 +29,43 @@ int main(int argc, char **argv, char **env)
  */
 void *shell_interprete(void)
 {
-        char *varChr_line = NULL;
-        char **varChr_args = NULL;
-        int varInt_status = 1;
+        ssize_t varSST_read = 0;
+        size_t varSTp_read_bytes = 0;
+        char * varChr_str = NULL;
+        
+        /* print a prompt */
+        write(1, "\033[1;36m$ \033[0m", 14);
 
-/*infinite loop waiting command or exit */
-        while (varInt_status) {
-/* print a prompt */
-        write(1, "$ ", 2);
-/* read the input line */
-        varChr_line = shell_read_line();
-/* separete the input line into tokens/arguments */
-        varChr_args = shell_tokenize_line(varChr_line);
-/* execute the command */
-        varInt_status = shell_execute_args(varChr_args);
-
-/* free the line */
-        free(varChr_line);
-        free(varChr_args);
-        }
+        varSTp_read_bytes = getline( &varChr_str, &varSST_read, stdin );
+        write(1, varChr_str, varSST_read);
+        
+        free(varChr_str);
 }
 
 char *shell_read_line(void)
 {
-        /* when NULL and getline allocate buffer auto */
-        char *varChr_line = NULL;
-        ssize_t varSST_buffer_size = 0;
+        ssize_t varSST_read = 0;
+        size_t varSTp_read_bytes = 0;
+        char * varChr_str = NULL;
 
-        if (getline(&varChr_line, &varSST_buffer_size, stdin) == -1)
-        {
-                /* Return the EOF indicator for STREAM.  */
-                if (feof(stdin))
-                {
-                        exit(EXIT_SUCCESS);
-                        }
-                        else
-                        {
-                                perror("shell_read_line");
-                                exit(EXIT_FAILURE);
-                                }
-                                }
-                                return varChr_line;
+        varSTp_read_bytes = getline( &varChr_str, &varSST_read, stdin );
+
+        write(1, varChr_str, varSST_read);
+        
+        free(varChr_str);
 }
 
 char **shell_tokenize_line(char *varChr_line)
 {
-
 }
 
-int lsh_launch(char **varChr_args)
+
+int shell_execute_args(char **varChr_args)
 {
 
 }
 
-int shell_execute_args(char **varChr_args)
+void shell_terminate(void)
 {
 
 }
