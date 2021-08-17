2cc0f994100e29274501059e041ea71d070af103
root <ralexrivero@gmail.com>
2021-08-17 11:28:11 -0700

    shell

--- prompt.c
+++ prompt.c
@@ -1,86 +1,91 @@
 #include "shell.h"
 
-/**
- * @brief prints a prompt at stdout
- * wait user input
- * execute the command
- * @argc: number of arguments passed at start the shell
- * @argv: pointer to array or arguments
- * @env: pointer to environment variables
- * @return int 0 when done
- */
-int main(int argc, char **argv, char **env)
-{
-        /* initialize the shell */
-        shell_initialize();
-        /* loop waiting for commands */
-        shell_interprete();
-        /* return 0 to terminate the shell and free */
-        shell_terminate();
-        return (EXIT_SUCCESS);
-}
+
+
+char *shell_input(void);
+char **shell_tokenize(char *input);
+int shell_execute(char **arguments);
 
 /**
- * @brief shell interprete
- * 
- * @return void* 
+ * main - entry point
  */
-void *shell_interprete(void)
+
+int main(void)
 {
-        char *varChr_line = NULL;
-        char **varChr_args = NULL;
-        int varInt_status = 1;
+char *input;
+char **arguments;
+int status;
+char prompt[] = "\033[1;36m$ \033[0m";
 
-/*infinite loop waiting command or exit */
-        while (varInt_status) {
-/* print a prompt */
-        write(1, "$ ", 2);
-/* read the input line */
-        varChr_line = shell_read_line();
-/* separete the input line into tokens/arguments */
-        varChr_args = shell_tokenize_line(varChr_line);
-/* execute the command */
-        varInt_status = shell_execute_args(varChr_args);
+	do {
+		write(1, prompt , 15);
+input = shell_input();
+printf("%s\n", input);
+arguments = shell_tokenize(input);
+/* status = shell_execute(arguments); */
 
-/* free the line */
-        free(varChr_line);
-        free(varChr_args);
-        }
+/* free(input); */
+/* free(arguments); */
+} while (status); 
 }
 
-char *shell_read_line(void)
-{
-        /* when NULL and getline allocate buffer auto */
-        char *varChr_line = NULL;
-        ssize_t varSST_buffer_size = 0;
 
-        if (getline(&varChr_line, &varSST_buffer_size, stdin) == -1)
-        {
-                /* Return the EOF indicator for STREAM.  */
-                if (feof(stdin))
-                {
-                        exit(EXIT_SUCCESS);
-                        }
-                        else
-                        {
-                                perror("shell_read_line");
-                                exit(EXIT_FAILURE);
-                                }
-                                }
-                                return varChr_line;
+/**
+ * shell_input
+ */
+char *shell_input(void)
+{
+	/* when NULL and getline allocate buffer auto */
+	char *input = NULL;
+	ssize_t buffer = 0;
+
+	if (getline(&input, &buffer, stdin) == -1)
+	{
+		/* Return the EOF indicator for STREAM.  */
+		if (feof(stdin))
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("Error on input line");
+			exit (EXIT_FAILURE);
+		}
+	}
+	return (input);
 }
 
-char **shell_tokenize_line(char *varChr_line)
+char **shell_tokenize(char *input)
 {
+	int bufsize = BUFFER_TOKEN, i = 0;
+	char **tokens = malloc(bufsize * sizeof(char*));
+	char *piece, **tokens_backup;
+	char delim[] = " ";
 
-}
+	if (!tokens) {
+		perror("malloc error 1");
+		exit(EXIT_FAILURE);
+	}
 
-int lsh_launch(char **varChr_args)
-{
+	piece = strtok(input, delim);
+	while (piece != NULL) {
+		tokens[i] = piece;
+		i++;
 
-}
+		if (i >= bufsize) {
+			bufsize += BUFFER_TOKEN;
+			tokens_backup = tokens;
+			tokens = realloc(tokens, bufsize * sizeof(char*));
+			if (!tokens) 
+				free(tokens_backup);
+			perror("malloc error 2");
+			exit(EXIT_FAILURE);
+		}
+	}
 
-int shell_execute_args(char **varChr_args)
-{
+	piece = strtok(NULL, delim);
 
+
+tokens[i] = NULL;
+return (tokens);
 }
