d0c250bee9d648e7fb0b12792ad971a1fa4af940
ralexrivero <ralexrivero@gmail.com>
2021-08-23 00:07:00 -0400

    prompt line ready

--- 01-prompt_line.c
+++ 01-prompt_line.c
@@ -1,13 +1,12 @@
 #include "shell.h"
 
+/**
+ * prompt_line - print the prompt simbol in color
+ * Description: set color, print and reset to default
+ * Return: always 0
+ */
 int prompt_line(void)
 {
-        _puts(BRAND PROMPT RESET);
-        return (0);
+	_puts(BRAND PROMPT RESET);
+	return (0);
 }
-
-/* int main(void)
-{
-        prompt_line();
-        return (0);
-} */
