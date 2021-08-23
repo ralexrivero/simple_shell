eba1945868d7b456cd602e0ea937fecdfb31f31a
ralexrivero <ralexrivero@gmail.com>
2021-08-23 00:20:12 -0400

    shell read

--- 01-shell_read.c
+++ 01-shell_read.c
@@ -1,34 +1,32 @@
 #include "shell.h"
 
+/**
+ * shell_read -
+ * 
+ * Return: char* 
+ */
 
 char *shell_read(void)
 {
 	char *input = NULL;
-        /* define and set to 0 to getline allocate memory */
-        size_t buffer_size = 0;
+	/* define and set to 0 to getline allocate memory */
+	size_t buffer_size = 0;
 
-        /* print prompt in color and reset color of input */
-        /* return -1 means failure reading line or reading EOF */
-        if (getline(&input, &buffer_size, stdin) == -1)
-        {
-                /* If receive and EOF (end of file) */
-                if (feof(stdin))
-                {
-                        exit(EXIT_SUCCESS);
-                }
-                else
-                {
-                        perror("getline failure\n");
-                        exit(EXIT_FAILURE);
-                }
-        }       
-        return (input);
-        /* returns input with an EOF */
+	/* print prompt in color and reset color of input */
+	/* return -1 means failure reading line or reading EOF */
+	if (getline(&input, &buffer_size, stdin) == -1)
+	{
+		/* If receive and EOF (end of file) */
+		if (feof(stdin))
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("getline failure\n");
+			exit(EXIT_FAILURE);
+		}
+	}       
+	return (input);
+	/* returns input with an EOF */
 }
-
-/* int main(void)
-{
-        char * typed = shell_read();
-        printf("typed: %s\n", typed);
-        return (0);
-} */
