c8c748205df7365dda779f73c36c5cf7469d4440
ralexrivero <ralexrivero@gmail.com>
2021-08-23 00:02:57 -0400

    betty

--- 00-simple_shell.c
+++ 00-simple_shell.c
@@ -1,10 +1,17 @@
 #include "shell.h"
-
+/**
+ * main -entry point
+ * Description: Simple Shell
+ * @argc: argument count, not used
+ * @argv: pointer to arguments, not used
+ * @envp: pointer to environment variables, used to get PATH
+ * Return: always 0
+ */
 int main(int argc, char **argv, char **envp)
 {
-        (void) argc;
-        (void) argv;
-        shell_init(envp);
+	(void) argc;
+	(void) argv;
+	shell_init(envp);
 
-        return (0);
+	return (0);
 }
