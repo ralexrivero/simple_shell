9b9d3eb77083c40ef98769ba6e59c790d5501f12
hippocampus3282 <3282@holbertonschool.com>
2021-08-25 19:23:12 -0700

    valgrind fix

--- s_strcat.c
+++ s_strcat.c
@@ -7,33 +7,25 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int len = 0;
-	int len2 = 0;
-	int total_len = 0;
-	int j = 0;
+	int i = 0, j = 0, res = 0;
+	char *str = NULL;
 
-	/* must realloc the total size of both string */
-	while (dest[len] != '\0')
-	{
-		len++;
-		total_len++;
-	}
-	while (src[len2] != '\0')
-	{
-		len2++;
-		total_len++;
-	}
-
-	/* realloc */
-	dest = realloc(dest, sizeof(char) * total_len + 1);
-
-	while (src[j] != '\0')
-	{
-		dest[len] = src[j];
-		len++;
-		j++;
-	}
-	dest[len] = '\0';
+	for (i = 0; dest[i]; i++)
+		;
+	/* first string - '\0' */
+	if (i)
+		i -= 1;
+	res = res + i;
+	for (i = 0; src[i]; i++)
+		;
+	/* add second string + '\0' */
+	res = res + i;
+	str = malloc(sizeof(char) * res);
+	for (i = 0; dest[i]; i++)
+		str[i] = dest[i];
+	for (j = 0; src[j]; i++, j++)
+		str[i] = src[j];
+	str[i] = '\0';
 
 	return (dest);
 }
