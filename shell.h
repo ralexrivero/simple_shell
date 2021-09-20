df2466f15312ef05d99e4b2b99848c1b15676f15
ralexrivero <ralexrivero@gmail.com>
2021-09-20 00:43:14 -0400

    The 033 way color

--- shell.h
+++ shell.h
@@ -15,6 +15,27 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+/**
+ * @brief prompt character and color style
+ * the \033[ way
+ * usage: WAY RED to print red
+ * usage: WAY B CYN bold cyan
+ */
+#define PROMPT "$ "
+#define WAY "\033["
+#define DEF "0m"
+#define BLK "30m"
+#define RED "31m"
+#define GRN "32m"
+#define YLW "33m"
+#define BLU "34m"
+#define MAG "35m"
+#define CYN "36m"
+#define WHI "37m"
+#define B "1;"
+#define BRAND WAY B CYN 
+#define RESET WAY DEF
+
 /*delimiter */
 #define ENVDELIM ":="
 
